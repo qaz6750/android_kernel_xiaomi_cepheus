278f751e524e039b7b58dfb19dcdd9e611f6d5f9
duckyduckG <duckyduckg65@gmail.com>
2025-07-30 21:51:21 +0500

    techpack: audio: sm8150: update snd info api
    Update snd info API and userspace and get codec info.
    
    Signed-off-by: duckyduckG <duckyduckg65@gmail.com>

--- techpack/audio/asoc/sm8150.c
+++ techpack/audio/asoc/sm8150.c
@@ -3958,6 +3958,23 @@ static struct notifier_block service_nb = {
 	.priority = -INT_MAX,
 };
 
+static struct snd_info_entry *msm_snd_info_create_subdir(struct module *mod,
+				const char *name,
+				struct snd_info_entry *parent)
+{
+	struct snd_info_entry *entry;
+
+	entry = snd_info_create_module_entry(mod, name, parent);
+	if (!entry)
+		return NULL;
+	entry->mode = S_IFDIR | 0555;
+	if (snd_info_register(entry) < 0) {
+		snd_info_free_entry(entry);
+		return NULL;
+	}
+	return entry;
+}
+
 static int msm_audrx_init(struct snd_soc_pcm_runtime *rtd)
 {
 	int ret = 0;
@@ -4087,7 +4104,7 @@ static int msm_audrx_init(struct snd_soc_pcm_runtime *rtd)
 			}
 		}
 		card = rtd->card->snd_card;
-		entry = snd_info_create_subdir(card->module, "codecs",
+		entry = msm_snd_info_create_subdir(card->module, "codecs",
 						 card->proc_root);
 		if (!entry) {
 			pr_debug("%s: Cannot create codecs module entry\n",
