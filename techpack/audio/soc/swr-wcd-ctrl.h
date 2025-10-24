e25e95a37bb34561bb467ee22b126d0a87c917c8
Pavel Dubrova <pashadubrova@gmail.com>
2025-10-24 14:50:01 +0500

    soc: swr-wcd: Fix CFI error due to missing core_vote member
    The exact number of members is required, otherwise we won't
    get a correct pointer to reg_irq.
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- techpack/audio/soc/swr-wcd-ctrl.h
+++ techpack/audio/soc/swr-wcd-ctrl.h
@@ -65,6 +65,7 @@ struct swr_ctrl_platform_data {
 	int (*write)(void *handle, int reg, int val);
 	int (*bulk_write)(void *handle, u32 *reg, u32 *val, size_t len);
 	int (*clk)(void *handle, bool enable);
+	int (*core_vote)(void *handle, bool enable);
 	int (*reg_irq)(void *handle, irqreturn_t(*irq_handler)(int irq,
 			void *data), void *swr_handle, int type);
 };
