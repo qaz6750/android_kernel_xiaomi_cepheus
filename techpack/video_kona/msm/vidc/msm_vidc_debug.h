5392b3621e7df61b2fc5c0473070ce1707548ece
duckyduckG <duckyduckg65@gmail.com>
2026-02-01 03:50:45 +0500

    techpack: video_kona: Update msm_vidc_events tracing and timer API
    Signed-off-by: duckyduckG <duckyduckg65@gmail.com>

--- techpack/video_kona/msm/vidc/msm_vidc_debug.h
+++ techpack/video_kona/msm/vidc/msm_vidc_debug.h
@@ -8,8 +8,18 @@
 #include <linux/debugfs.h>
 #include <linux/delay.h>
 #include <linux/types.h>
+#include "msm_vidc_events.h"
 #include "msm_vidc_internal.h"
-#include "trace/events/msm_vidc_events.h"
+
+/* Mock all the missing parts for successful compilation starts here */
+#include <linux/types.h>
+#include <linux/time.h>
+#include <linux/interrupt.h>
+#include <soc/qcom/subsystem_restart.h>
+#include "msm_vidc_internal.h"
+
+#define SMEM_IMAGE_VERSION_TABLE 469
+/* Mock all the missing parts for successful compilation ends */
 
 #ifndef VIDC_DBG_LABEL
 #define VIDC_DBG_LABEL "msm_vidc"
@@ -242,7 +252,7 @@ static inline void tic(struct msm_vidc_inst *i, enum profiling_points p,
 		memcpy(i->debug.pdata[p].name, b, 64);
 	if ((msm_vidc_debug & VIDC_PERF) &&
 		i->debug.pdata[p].sampling) {
-		do_gettimeofday(&__ddl_tv);
+		__ddl_tv = ktime_to_timeval(ktime_get_real());
 		i->debug.pdata[p].start =
 			(__ddl_tv.tv_sec * 1000) + (__ddl_tv.tv_usec / 1000);
 			i->debug.pdata[p].sampling = false;
@@ -255,7 +265,7 @@ static inline void toc(struct msm_vidc_inst *i, enum profiling_points p)
 
 	if ((msm_vidc_debug & VIDC_PERF) &&
 		!i->debug.pdata[p].sampling) {
-		do_gettimeofday(&__ddl_tv);
+		__ddl_tv = ktime_to_timeval(ktime_get_real());
 		i->debug.pdata[p].stop = (__ddl_tv.tv_sec * 1000)
 			+ (__ddl_tv.tv_usec / 1000);
 		i->debug.pdata[p].cumulative += i->debug.pdata[p].stop -
