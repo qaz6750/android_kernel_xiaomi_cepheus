fd3c6f4d1701b7532084eb32eada950f7e71331b
duckyduckG <duckyduckg65@gmail.com>
2026-02-14 23:28:37 +0500

    usb: Import Xiaomi changes
    * changes for configfs.c are dropped
    
    Signed-off-by: duckyduckg <duckyduckg65@gmail.com>

--- drivers/usb/dwc3/dwc3-msm.c
+++ drivers/usb/dwc3/dwc3-msm.c
@@ -5724,8 +5724,6 @@ static int dwc3_msm_gadget_vbus_draw(struct dwc3_msm *mdwc, unsigned int mA)
 		 * bail out if suspend happened with float cable
 		 * connected
 		 */
-		if (mA == 2)
-			return 0;
 
 		if (!mA)
 			pval.intval = -ETIMEDOUT;
