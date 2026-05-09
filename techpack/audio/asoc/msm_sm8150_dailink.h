d39ec1c285c49d0779662e6a9bbb8d132d0221c5
duckyduckg <duckyduckg65@gmail.com>
2026-05-09 16:27:49 +0500

    techpack: audio: sm8150: Update tfa98xx dai_links for 5.4 kernel
    Signed-off-by: duckyduckg <duckyduckg65@gmail.com>

--- techpack/audio/asoc/msm_sm8150_dailink.h
+++ techpack/audio/asoc/msm_sm8150_dailink.h
@@ -4,6 +4,9 @@
  */
 
 #include <sound/soc.h>
+#ifdef CONFIG_SND_SOC_TFA9874_FOR_DAVI
+#include "codecs/tfa98xx/inc/tfa_platform_interface_definition.h"
+#endif
 
 /* FrontEnd DAI Links */
 SND_SOC_DAILINK_DEFS(multimedia1,
@@ -185,10 +188,17 @@ SND_SOC_DAILINK_DEFS(slimbus8_hostless,
 	DAILINK_COMP_ARRAY(COMP_CODEC("snd-soc-dummy", "snd-soc-dummy-dai")),
 	DAILINK_COMP_ARRAY(COMP_PLATFORM("msm-pcm-hostless")));
 
+#ifdef CONFIG_SND_SOC_TFA9874_FOR_DAVI
+SND_SOC_DAILINK_DEFS(tfa_tx_hostless,
+	DAILINK_COMP_ARRAY(COMP_CPU("TFA_TX_HOSTLESS_CPU_DAI_NAME")),
+	DAILINK_COMP_ARRAY(COMP_CODEC("snd-soc-dummy", "snd-soc-dummy-dai")),
+	DAILINK_COMP_ARRAY(COMP_PLATFORM("msm-pcm-hostless")));
+#else
 SND_SOC_DAILINK_DEFS(slimbus_4_tx,
 	DAILINK_COMP_ARRAY(COMP_CPU("msm-dai-q6-dev.16393")),
 	DAILINK_COMP_ARRAY(COMP_CODEC("tavil_codec", "tavil_vifeedback")),
 	DAILINK_COMP_ARRAY(COMP_PLATFORM("msm-pcm-hostless")));
+#endif
 
 /* Ultrasound RX DAI Link */
 SND_SOC_DAILINK_DEFS(slimbus_2_hostless,
@@ -480,7 +490,7 @@ SND_SOC_DAILINK_DEFS(tert_mi2s_tx,
 
 SND_SOC_DAILINK_DEFS(quat_mi2s_rx,
 	DAILINK_COMP_ARRAY(COMP_CPU("msm-dai-q6-mi2s.6")),
-	DAILINK_COMP_ARRAY(COMP_CODEC("msm-stub-codec.1", "msm-stub-rx")),
+	DAILINK_COMP_ARRAY(COMP_CODEC("tfa98xx.1-0034", "tfa98xx-aif-1-34")),
 	DAILINK_COMP_ARRAY(COMP_PLATFORM("msm-pcm-routing")));
 
 SND_SOC_DAILINK_DEFS(quat_mi2s_tx,
--- techpack/audio/asoc/sm8150.c
+++ techpack/audio/asoc/sm8150.c
@@ -5734,8 +5734,6 @@ static struct snd_soc_dai_link msm_tavil_fe_dai_links[] = {
 	{
 		.name = TFA_TX_HOSTLESS_CODEC_NAME,
 		.stream_name = TFA_TX_HOSTLESS_STREAM_NAME,
-		.cpu_dai_name = TFA_TX_HOSTLESS_CPU_DAI_NAME,
-		.platform_name	= "msm-pcm-hostless",
 		.dynamic = 1,
 		.dpcm_capture = 1,
 		.trigger = {SND_SOC_DPCM_TRIGGER_POST,
@@ -5745,8 +5743,7 @@ static struct snd_soc_dai_link msm_tavil_fe_dai_links[] = {
 		/* this dailink has playback support */
 		.ignore_pmdown_time = 1,
 		/* This dainlink has MI2S support */
-		.codec_dai_name = "snd-soc-dummy-dai",
-		.codec_name = "snd-soc-dummy",
+		SND_SOC_DAILINK_REG(tfa_tx_hostless),
 	},
 #else
 	{
@@ -6472,10 +6469,6 @@ static struct snd_soc_dai_link quat_mi2s_rx_tfa9874_dai_links[] = {
 	{
 		.name = LPASS_BE_QUAT_MI2S_RX,
 		.stream_name = "Quaternary MI2S Playback",
-		.cpu_dai_name = "msm-dai-q6-mi2s.3",
-		.platform_name = "msm-pcm-routing",
-		.codec_name = "tfa98xx.1-0034",
-		.codec_dai_name = "tfa98xx-aif-1-34",
 		.dynamic_be = 1,
 		.no_pcm = 1,
 		.dpcm_playback = 1,
@@ -6484,6 +6477,7 @@ static struct snd_soc_dai_link quat_mi2s_rx_tfa9874_dai_links[] = {
 		.ops = &msm_mi2s_be_ops,
 		.ignore_suspend = 1,
 		.ignore_pmdown_time = 1,
+		SND_SOC_DAILINK_REG(quat_mi2s_rx),
 	},
 };
 
